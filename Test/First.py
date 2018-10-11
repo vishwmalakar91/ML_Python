382f285d92325e50aaa631edd7f4cbcea739eb15
vishwanath malakar <vishwanathmalakar@gmail.com>
2018-10-11 19:35:38 +0530

    first commit

---
+++ Test/First.py
@@ -0,0 +1,7 @@
+from sklearn.tree import tree
+
+features =[[140,1],[130,1],[150,0],[170,0]]
+labels=["apple","apple","orange","orange"]
+clf=tree.DecisionTreeClassifier()
+clf=clf.fit(features, labels)
+print(clf.predict([[150,0]]))
